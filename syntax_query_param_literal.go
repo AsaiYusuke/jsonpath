af1eb8b7e1eda64140e58283c23bb787e5f992a2
Y.ASAI <prometric2000@gmail.com>
2020-12-09 15:07:48 -0800

    refactor

--- syntax_query_param_literal.go
+++ syntax_query_param_literal.go
@@ -5,7 +5,5 @@ type syntaxQueryParamLiteral struct {
 }
 
 func (l syntaxQueryParamLiteral) compute(root interface{}, currentMap map[int]interface{}) map[int]interface{} {
-	result := make(map[int]interface{}, 0)
-	result[0] = l.literal
-	return result
+	return map[int]interface{}{0: l.literal}
 }
