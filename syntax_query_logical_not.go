29bca9d4f1cdff06c487b4fab62cc2d8969b79d3
Y.ASAI <prometric2000@gmail.com>
2020-12-20 18:27:21 -0800

    fix logical not

--- syntax_query_logical_not.go
+++ syntax_query_logical_not.go
@@ -6,10 +6,11 @@ type syntaxLogicalNot struct {
 
 func (l *syntaxLogicalNot) compute(currentMap map[int]interface{}) map[int]interface{} {
 	computedMap := l.param.compute(currentMap)
-	resultMap := currentMap
-	for index := range computedMap {
-		delete(resultMap, index)
+	resultMap := make(map[int]interface{}, 0)
+	for index, value := range currentMap {
+		if _, ok := computedMap[index]; !ok {
+			resultMap[index] = value
+		}
 	}
-
 	return resultMap
 }
