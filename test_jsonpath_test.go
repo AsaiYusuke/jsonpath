cb61b1a7a440e237fab3a1f42c2668e1774acaf3
Y.ASAI <prometric2000@gmail.com>
2020-12-27 23:24:57 -0800

    add testcase

--- test_jsonpath_test.go
+++ test_jsonpath_test.go
@@ -4912,10 +4912,60 @@ func TestRetrieve_configAccessorMode(t *testing.T) {
 	execTestRetrieveTestGroups(t, testGroups)
 }
 
+func TestRetrieveExecTwice(t *testing.T) {
+	jsonpath1 := `$.a`
+	srcJSON1 := `{"a":123}`
+	expectedOutput1 := "[123]"
+	jsonpath2 := `$[1].b`
+	srcJSON2 := `[123,{"b":456}]`
+	expectedOutput2 := "[456]"
+
+	var src1 interface{}
+	if err := json.Unmarshal([]byte(srcJSON1), &src1); err != nil {
+		t.Errorf("%w", err)
+		return
+	}
+	var src2 interface{}
+	if err := json.Unmarshal([]byte(srcJSON2), &src2); err != nil {
+		t.Errorf("%w", err)
+		return
+	}
+
+	actualObject1, err := Retrieve(jsonpath1, src1)
+	if err != nil {
+		t.Errorf("expected error<nil> != actual error<%s>\n", err)
+		return
+	}
+	actualObject2, err := Retrieve(jsonpath2, src2)
+	if err != nil {
+		t.Errorf("expected error<nil> != actual error<%s>\n", err)
+		return
+	}
+
+	actualOutputJSON1, err := json.Marshal(actualObject1)
+	if err != nil {
+		t.Errorf("%w", err)
+		return
+	}
+	actualOutputJSON2, err := json.Marshal(actualObject2)
+	if err != nil {
+		t.Errorf("%w", err)
+		return
+	}
+
+	if string(actualOutputJSON1) != string(expectedOutput1) || string(actualOutputJSON2) != string(expectedOutput2) {
+		t.Errorf("actualOutputJSON1<%s> != expectedOutput1<%s> || actualOutputJSON2<%s> != expectedOutput2<%s>\n",
+			string(actualOutputJSON1), string(expectedOutput1), string(actualOutputJSON2), string(expectedOutput2))
+		return
+	}
+}
+
 func TestParserFuncExecTwice(t *testing.T) {
 	jsonpath := `$.a`
 	srcJSON1 := `{"a":1}`
+	expectedOutput1 := "[1]"
 	srcJSON2 := `{"a":2}`
+	expectedOutput2 := "[2]"
 
 	var src1 interface{}
 	if err := json.Unmarshal([]byte(srcJSON1), &src1); err != nil {
@@ -4956,9 +5006,9 @@ func TestParserFuncExecTwice(t *testing.T) {
 		return
 	}
 
-	if string(actualOutputJSON1) == string(actualOutputJSON2) {
-		t.Errorf("actualOutputJSON1<%s> == expectedOutputJSON2<%s>\n",
-			string(actualOutputJSON1), string(actualOutputJSON2))
+	if string(actualOutputJSON1) != string(expectedOutput1) || string(actualOutputJSON2) != string(expectedOutput2) {
+		t.Errorf("actualOutputJSON1<%s> != expectedOutput1<%s> || actualOutputJSON2<%s> != expectedOutput2<%s>\n",
+			string(actualOutputJSON1), string(expectedOutput1), string(actualOutputJSON2), string(expectedOutput2))
 		return
 	}
 }
