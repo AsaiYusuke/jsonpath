ba04e589f84b586385d0613746ac3c1302d66a70
Yusuke Asai <prometric2000@gmail.com>
2023-10-26 23:15:53 +0900

    remove unused number check

--- syntax_basic_type_validator_any_value.go
+++ syntax_basic_type_validator_any_value.go
@@ -1,19 +1,12 @@
 package jsonpath
 
-import "encoding/json"
-
 type syntaxBasicAnyValueTypeValidator struct {
 }
 
 func (c *syntaxBasicAnyValueTypeValidator) validate(values []interface{}) bool {
 	var foundValue bool
 	for index := range values {
-		switch typedValue := values[index].(type) {
-		case json.Number:
-			foundValue = true
-			values[index], _ = typedValue.Float64()
-		case struct{}:
-		default:
+		if values[index] != emptyEntity {
 			foundValue = true
 		}
 	}
