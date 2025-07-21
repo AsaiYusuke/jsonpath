dc9d4b93b312394f646b9198e4cfc49e304cae83
Yusuke Asai <prometric2000@gmail.com>
2025-07-21 17:26:13 +0900

    refactor: simplify errorRuntimeAdapter structure by embedding errorBasicRuntime

--- internal/syntax/error_adapter.go
+++ internal/syntax/error_adapter.go
@@ -3,31 +3,27 @@ package syntax
 import "github.com/AsaiYusuke/jsonpath/errors"
 
 type errorRuntimeAdapter struct {
-	err  error
-	node *syntaxBasicNode
-}
-
-func (e errorRuntimeAdapter) getSyntaxNode() *syntaxBasicNode {
-	return e.node
+	*errorBasicRuntime
+	err error
 }
 
 func newErrorTypeUnmatched(node *syntaxBasicNode, expectedType, foundType string) errorRuntime {
 	return errorRuntimeAdapter{
-		err:  errors.NewErrorTypeUnmatched(node.text, expectedType, foundType),
-		node: node,
+		errorBasicRuntime: &errorBasicRuntime{node: node},
+		err:               errors.NewErrorTypeUnmatched(node.text, expectedType, foundType),
 	}
 }
 
 func newErrorMemberNotExist(node *syntaxBasicNode) errorRuntime {
 	return errorRuntimeAdapter{
-		err:  errors.NewErrorMemberNotExist(node.text),
-		node: node,
+		errorBasicRuntime: &errorBasicRuntime{node: node},
+		err:               errors.NewErrorMemberNotExist(node.text),
 	}
 }
 
 func newErrorFunctionFailed(node *syntaxBasicNode, errorString string) errorRuntime {
 	return errorRuntimeAdapter{
-		err:  errors.NewErrorFunctionFailed(node.text, errorString),
-		node: node,
+		errorBasicRuntime: &errorBasicRuntime{node: node},
+		err:               errors.NewErrorFunctionFailed(node.text, errorString),
 	}
 }
