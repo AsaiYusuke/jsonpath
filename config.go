a82abdb4b7cba807aeec75f583b9ba314216e384
Asai Yusuke <AsaiYusuke@users.noreply.github.com>
2025-08-20 00:07:08 +0900

    feat: refactor config structure (#56)
    * feat: refactor config structure
    
    * fix: update coverpkg in unit test command to include config package
    
    * fix: update example links in README to point to v2 documentation

--- config.go
+++
@@ -1,5 +0,0 @@
-package jsonpath
-
-import "github.com/AsaiYusuke/jsonpath/v2/internal/syntax"
-
-type Config = syntax.Config
--- jsonpath.go
+++ jsonpath.go
@@ -1,13 +1,14 @@
 package jsonpath
 
 import (
+	"github.com/AsaiYusuke/jsonpath/v2/config"
 	"github.com/AsaiYusuke/jsonpath/v2/internal/syntax"
 )
 
-func Retrieve(jsonPath string, src any, config ...Config) ([]any, error) {
+func Retrieve(jsonPath string, src any, config ...config.Config) ([]any, error) {
 	return syntax.Retrieve(jsonPath, src, config...)
 }
 
-func Parse(jsonPath string, config ...Config) (func(src any) ([]any, error), error) {
+func Parse(jsonPath string, config ...config.Config) (func(src any) ([]any, error), error) {
 	return syntax.Parse(jsonPath, config...)
 }
